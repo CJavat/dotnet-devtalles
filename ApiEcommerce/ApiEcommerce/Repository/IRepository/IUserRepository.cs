4f3ca2eef68aff56a65a206bc5607a3a9905ea7b
caplascencia <ca.plascencia@mavi.mx>
2025-07-25 09:33:16 -0600

    SECCIÓN 8 / CAP 6 - EN CURSO

---
+++ ApiEcommerce/ApiEcommerce/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,24 @@
+using ApiEcommerce.Models;
+using ApiEcommerce.Models.Dtos;
+
+namespace ApiEcommerce.Repository.IRepository;
+/*
+=============
+🏆 Ejercicio 
+=============
+*/
+// 2. Incluir los siguientes métodos en la interfaz:
+//    - Login
+//        → Recibe un objeto UserLoginDto y devuelve un UserLoginResponseDto de forma asíncrona (Task).
+//
+//    - Register
+//        → Recibe un objeto CreateUserDto y devuelve un objeto User de forma asíncrona (Task).
+
+public interface IUserRepository
+{
+  ICollection<User> GetUsers();
+  User? GetUser(int id);
+  bool IsUniqueUser(string name);
+  Task<UserLoginResponseDto> Login(UserLoginDto userLogin);
+  Task<User> Register(CreateUserDto createUserDto);
+}
