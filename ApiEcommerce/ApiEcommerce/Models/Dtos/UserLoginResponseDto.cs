c8b60cfdc196c502f07662064221b73a945f5ea3
CJavatX <cdpm98@hotmail.com>
2025-11-28 07:30:54 -0600

    SECCIÓN 8 - TERMINADO(R)

--- ApiEcommerce/ApiEcommerce/Models/Dtos/UserLoginResponseDto.cs
+++ ApiEcommerce/ApiEcommerce/Models/Dtos/UserLoginResponseDto.cs
@@ -2,7 +2,7 @@ namespace ApiEcommerce.Models.Dtos;
 
 public class UserLoginResponseDto
 {
-  public UserLoginDto? User { get; set; }
+  public UserRegisterDto? User { get; set; }
   public string? Token { get; set; }
   public string? Message { get; set; }
 }
